1ef1cd457327a9fdce9908077c12528ef2ec8432
Jan Melewski <melewskijan@gmail.com>
2024-05-22 23:39:41 +0200

    dodanie czesci bibliotek, czcionki wstepnej oraz kolorow interfejsu

---
+++ dziennik_cisnieniowca.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox, filedialog
+import csv
+import os
+#Wstępne biblioteki potrzebne do pisania kodu
+
+# Czcionka
+FONT = ("Helvetica", 12)
+
+# Kolory
+BG_COLOR = "#f0f0f0"
+BUTTON_COLOR = "#ADD8E6"
+TEXT_COLOR = "#333333"
