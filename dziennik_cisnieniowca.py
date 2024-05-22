7140c9f08449b994b351e4ed502e2cf8051048db
Jan Melewski <melewskijan@gmail.com>
2024-05-23 00:14:26 +0200

    dodanie przycisku wyjscia oraz zmiany nazw innych przyciskow, dla przejrzystosci

--- dziennik_cisnieniowca.py
+++ dziennik_cisnieniowca.py
@@ -24,7 +24,7 @@ class Dziennik_Cisnieniowca_Aplikacja:
 
         # Tytuł okna
         self.title_label = tk.Label(self.master, text="Dziennik Ciśnieniowca", font=TITLE_FONT, bg=BG_COLOR, fg=TITLE_COLOR)
-        self.title_label.pack(pady=40)
+        self.title_label.pack(pady=30)
 
         self.main_frame = tk.Frame(self.master, bg=BG_COLOR)
         self.main_frame.pack(expand=True)
@@ -33,17 +33,21 @@ class Dziennik_Cisnieniowca_Aplikacja:
                                     font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
         self.add_button.pack(pady=10)
 
-        self.add_button = tk.Button(self.main_frame, text="Szukaj pomiaru", command=self,
+        self.search_button = tk.Button(self.main_frame, text="Szukaj pomiaru", command=self,
                                     font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
-        self.add_button.pack(pady=10)
+        self.search_button.pack(pady=10)
 
-        self.add_button = tk.Button(self.main_frame, text="Zmien ścieżkę zapisu", command=self,
+        self.change_button = tk.Button(self.main_frame, text="Zmien ścieżkę zapisu", command=self,
                                     font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
-        self.add_button.pack(pady=10)
+        self.change_button.pack(pady=10)
 
-        self.add_button = tk.Button(self.main_frame, text="Utwórz wykres pomiarów", command=self,
+        self.chart_button = tk.Button(self.main_frame, text="Utwórz wykres pomiarów", command=self,
                                     font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
-        self.add_button.pack(pady=10)
+        self.chart_button.pack(pady=10)
+
+        self.quit_button = tk.Button(self.main_frame, text="Wyjscie", command=self.master.quit,
+                                     font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
+        self.quit_button.pack(pady=10)
 
 def main():
     root = tk.Tk()
