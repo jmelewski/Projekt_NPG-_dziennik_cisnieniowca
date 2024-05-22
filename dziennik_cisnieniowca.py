ce157bf18491650b8a860173268ae9dd8d2f83c1
Jan Melewski <melewskijan@gmail.com>
2024-05-23 00:09:56 +0200

    dodanie pozostalych przyciskow oraz wyrownanie tytulu

--- dziennik_cisnieniowca.py
+++ dziennik_cisnieniowca.py
@@ -6,7 +6,7 @@ import os
 
 # Czcionka
 FONT = ("Helvetica", 13)
-TITLE_FONT = ("Helvetica", 18, "bold")
+TITLE_FONT = ("Helvetica", 22, "bold")
 
 # Kolory
 BG_COLOR = "#fbaed2"
@@ -24,12 +24,24 @@ class Dziennik_Cisnieniowca_Aplikacja:
 
         # Tytuł okna
         self.title_label = tk.Label(self.master, text="Dziennik Ciśnieniowca", font=TITLE_FONT, bg=BG_COLOR, fg=TITLE_COLOR)
-        self.title_label.pack(pady=10)
+        self.title_label.pack(pady=40)
 
         self.main_frame = tk.Frame(self.master, bg=BG_COLOR)
         self.main_frame.pack(expand=True)
         #Przyciski
-        self.add_button = tk.Button(self.main_frame, text="Nowy Pomiar", command=self,
+        self.add_button = tk.Button(self.main_frame, text="Nowy pomiar", command=self,
+                                    font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
+        self.add_button.pack(pady=10)
+
+        self.add_button = tk.Button(self.main_frame, text="Szukaj pomiaru", command=self,
+                                    font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
+        self.add_button.pack(pady=10)
+
+        self.add_button = tk.Button(self.main_frame, text="Zmien ścieżkę zapisu", command=self,
+                                    font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
+        self.add_button.pack(pady=10)
+
+        self.add_button = tk.Button(self.main_frame, text="Utwórz wykres pomiarów", command=self,
                                     font=FONT, bg=BUTTON_COLOR, fg=TEXT_COLOR)
         self.add_button.pack(pady=10)
 
