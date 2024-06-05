8719b3caef2e2cd8086b636eb38dd2830fd2dee9
g0fry <164173094+g0fry@users.noreply.github.com>
2024-06-05 15:53:46 +0200

    dodanie ostrzeżenia przy wczytaniu odbiegających od normy pomiarów

--- dziennik_cisnieniowca.py
+++ dziennik_cisnieniowca.py
@@ -282,11 +282,29 @@ class Dziennik_Cisnieniowca_Aplikacja:
         pressure1 = self.systolic_entry.get()
         pressure2 = self.diastolic_entry.get()
         pulse = self.pulse_entry.get()
+        self.check_levels(pressure1, pressure2, pulse)
         measurement_data = f"{date}; {time}; {pressure1}; {pressure2}; {pulse}\n"
         with open(self.current_file, "a") as file:
             file.write(measurement_data)
         self.measurements.append((date, time, pressure1, pressure2, pulse))
         self.add_window.destroy()
+
+    def check_levels(self, systolic, diastolic, pulse):
+        systolic = int(systolic)
+        diastolic = int(diastolic)
+        pulse = int(pulse)
+        if (systolic > 140 or diastolic > 90) and pulse > 100:
+            messagebox.showwarning("Ostrzeżenie", "Uwaga - podwyższone ciśnienie oraz puls.")
+        elif (systolic < 120 or diastolic < 80) and pulse < 60: 
+            messagebox.showwarning("Ostrzeżenie", "Uwaga - zaniżone ciśnienie oraz puls.")
+        elif systolic > 140 or diastolic > 90:
+            messagebox.showwarning("Ostrzeżenie", "Uwaga - podwyższone ciśnienie.")
+        elif systolic < 120 or diastolic < 80:
+            messagebox.showwarning("Ostrzeżenie", "Uwaga - zaniżone ciśnienie.")
+        elif pulse > 100:
+            messagebox.showwarning("Ostrzeżenie", "Uwaga - podwyższony puls.")
+        elif pulse < 60:
+            messagebox.showwarning("Ostrzeżenie", "Uwaga - zaniżony puls.")
 
 
 # Main function to run the application
